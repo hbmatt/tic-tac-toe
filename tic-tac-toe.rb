10486a056ae573c7e9024b433ac126b80dcda4b7
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-09-07 15:17:01 -0500

    Fix bug

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -64,7 +64,7 @@ class TicTacToe
   end
 
   def winning_move?(moves)
-    winning_cells = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 4], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    winning_cells = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     winning_cells.any? { |set| set - moves == [] }
   end
 
